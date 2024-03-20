d4fba5f5ad912609b78ad0d3b3108615a009ebfc
h7yt0p <h7yt0p@duck.com>
2024-03-20 20:15:32 +0300

    creation of base seed

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// include section
+#include <iostream>
+#include <random>
+//include section end
+
+//namespace section
+using namespace std;
+//namespace section end
+
+//variables section
+int baseSeed;
+//variables section end
+
+//functions section
+baseSeedCreator() {
+baseSeed = rand();
+}
+//functions section end
+
+//main function section
+int main() {
+
+    cout << baseSeed << endl;
+
+    return 0;
+}
+//main function section end
+
