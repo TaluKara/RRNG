b30dfb7bde3683a5a0f5e4f6da6891dafba05f38
h7yt0p <h7yt0p@duck.com>
2024-03-20 21:11:30 +0300

    it is done.

--- main.cpp
+++ main.cpp
@@ -20,13 +20,13 @@ int main() {
     srand(time(0));
 
     int complexity;
-    std::cout << "Oluşturulacak sayı ne kadar karmaşık olsun?\n"
-              << "1) Zayıf\n"
-              << "2) İdare Eder\n"
-              << "3) Orta\n"
-              << "4) Güçlü\n"
-              << "5) Çok Güçlü\n"
-              << "Seçiminiz: ";
+    std::cout << "How complex should the generated number be?\n"
+              << "1) Weak\n"
+              << "2) Fair\n"
+              << "3) Medium\n"
+              << "4) Strong\n"
+              << "5) Very Strong\n"
+              << "Your choice: ";
     std::cin >> complexity;
 
     int x;
@@ -47,22 +47,23 @@ int main() {
             x = 2000 + rand() % (5000 - 2000 + 1);
             break;
         default:
-            std::cout << "Geçersiz seçim. Programdan çıkılıyor...";
+            std::cout << "Invalid choice. Exiting the program...";
             return 1;
     }
-    std::cout << "Fonksiyonların tekrar sayısı: " << x << std::endl;
 
     int y;
-    std::cout << "Kaç basamaklı bir sayı oluşturmak istiyorsunuz: ";
+    std::cout << "How many digits should the number have: ";
     std::cin >> y;
 
     long long seed = calculateSeed(x);
+    std::string lastRandomNumber;
 
     for(int i = 0; i < x; ++i) {
-        std::string randomNumber = generateRandomNumber(seed, y);
-        std::cout << i+1 << ". Üretilen sayı: " << randomNumber << std::endl;
-        seed = std::stoll(randomNumber.substr(0, randomNumber.length() < 10 ? randomNumber.length() : 10)) % 100000;
+        lastRandomNumber = generateRandomNumber(seed, y);
+        seed = std::stoll(lastRandomNumber.substr(0, lastRandomNumber.length() < 10 ? lastRandomNumber.length() : 10)) % 100000;
     }
+
+    std::cout << "The last generated number: " << lastRandomNumber << std::endl;
 
     return 0;
 }
