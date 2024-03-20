23ebf14ab72b1a9c064786b484bd7a857802ad64
h7yt0p <h7yt0p@duck.com>
2024-03-20 20:35:15 +0300

    doing pretty well. im close.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,45 @@
-// include section
 #include <iostream>
 #include <random>
-//include section end
 
-//namespace section
 using namespace std;
-//namespace section end
 
-//variables section
-int baseSeed;
-//variables section end
+// Function to generate a random number between 10 and 20
+int generateInitialRandomNumber() {
+    // Create a random device
+    random_device rd;
+    // Initialize a Mersenne Twister pseudo-random number generator
+    mt19937 gen(rd());
+    // Uniform distribution between 10 and 20
+    uniform_int_distribution<> distr(10, 20);
 
-//functions section
-baseSeedCreator() {
-baseSeed = rand();
+    // Generate and return a random number
+    return distr(gen);
 }
-//functions section end
 
-//main function section
+// Function to generate a random number using a custom seed
+int generateRandomNumberWithCustomSeed(unsigned int seed) {
+    // Initialize a Mersenne Twister pseudo-random number generator with the custom seed
+    mt19937 gen(seed);
+    // Uniform distribution between 10 and 20
+    uniform_int_distribution<> distr(10, 20);
+
+    // Generate and return a random number
+    return distr(gen);
+}
+
 int main() {
+    // Generate an initial random number x between 10 and 20
+    int x = generateInitialRandomNumber();
+    cout << "Initial random number (x): " << x << endl;
 
-    cout << baseSeed << endl;
+    // Loop x times
+    for (int i = 0; i < x; ++i) {
+        // Generate a new seed for each iteration
+        unsigned int newSeed = generateInitialRandomNumber();
+        // Use the new seed to generate a random number
+        int randomNumber = generateRandomNumberWithCustomSeed(newSeed);
+        cout << "Random number in iteration " << i+1 << ": " << randomNumber << endl;
+    }
 
     return 0;
 }
-//main function section end
-
